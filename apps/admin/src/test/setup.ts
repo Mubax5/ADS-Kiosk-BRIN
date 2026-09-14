55b2603290e88263e98c046e939da77d1503897f
Muba <140395247+Mubax5@users.noreply.github.com>
2026-09-14 11:36:15 +0700

    test: provide browser media query stub

--- apps/admin/src/test/setup.ts
+++ apps/admin/src/test/setup.ts
@@ -5,4 +5,23 @@ class ResizeObserverStub {
   unobserve() {}
   disconnect() {}
 }
-Object.defineProperty(globalThis, "ResizeObserver", { value: ResizeObserverStub, configurable: true });
+
+Object.defineProperty(globalThis, "ResizeObserver", {
+  value: ResizeObserverStub,
+  configurable: true,
+});
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  configurable: true,
+  value: (query: string): MediaQueryList => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => undefined,
+    removeListener: () => undefined,
+    addEventListener: () => undefined,
+    removeEventListener: () => undefined,
+    dispatchEvent: () => false,
+  }),
+});
